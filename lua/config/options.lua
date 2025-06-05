56a99db2eee19b0d4dd387b5d7bd8f966a0f54cc
13byte <13bitadc@naver.com>
2025-06-05 22:39:19 +0900

    .

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Python LSP로 pyrefly만 사용
-vim.g.lazyvim_python_lsp = "pyrefly"
--- Python formatter(포매터)로 ruff만 사용
+vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_python_ruff = "ruff"
