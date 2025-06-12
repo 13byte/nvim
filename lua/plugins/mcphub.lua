b8f40b280fdd6f4937a37ca45b040ad028a5b393
13byte <13bitadc@naver.com>
2025-06-12 11:14:35 +0900

    add: mcphub

---
+++ lua/plugins/mcphub.lua
@@ -0,0 +1,18 @@
+return {
+  "ravitemer/mcphub.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "yetone/avante.nvim",
+  },
+  build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+  config = function()
+    require("mcphub").setup({
+      auto_approve = true, -- 선택사항: 자동 승인
+      extensions = {
+        avante = {
+          make_slash_commands = true,
+        },
+      },
+    })
+  end,
+}
