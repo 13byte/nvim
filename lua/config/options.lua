caa1f48f42e03e38cb42461fe29a3a107b78c3af
13byte <13bitadc@naver.com>
2025-06-15 19:49:32 +0900

    .

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Python LSP 설정
 vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_python_ruff = "ruff"
+
+-- Edge 테마를 기본 colorscheme으로 설정
+vim.g.lazyvim_colorscheme = "edge"
