892e9c49577550fa43b489ca3be53a7684f01cab
13byte <13bitadc@naver.com>
2025-09-06 21:20:50 +0900

    .

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,8 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
--- Python LSP 설정
 vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_python_ruff = "ruff"
+
+vim.g.lazyvim_python_ruff_config = {
+  line_length = 88,
+  format_on_save = true,
+  organize_imports = true,
+}
